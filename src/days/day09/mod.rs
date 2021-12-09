79ac02fb201864448555e144acb65bbf23f2f0fb
Denis BOURGE <bourge.denis@gmail.com>
2021-12-09 15:29:45 +0100

    Part 2 ok.

--- src/days/day09/mod.rs
+++ src/days/day09/mod.rs
@@ -66,6 +66,16 @@
 //! Find the three largest basins and multiply their sizes together. In the above example, this is 9 * 14 * 9 = 1134.
 //!
 //! What do you get if you multiply together the sizes of the three largest basins?
+//!
+//! Your puzzle answer was 821560.
+
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+};
+
+use image::ImageBuffer;
+use itertools::Itertools;
 
 use crate::{day::Challenge, parse_input_raw};
 
@@ -78,19 +88,90 @@ struct HeightMap {
     data: Vec<u32>,
 }
 
+fn state_to_color(state: u32) -> &'static [u8; 3] {
+    // Palette from https://coolors.co/d5bff2-d3bae3-d1b6d6-b197bd-706791-4e507a-364066-1d3057-10274f-031e47
+    match state {
+        9 => &[213, 191, 242],
+        8 => &[211, 186, 227],
+        7 => &[209, 182, 214],
+        6 => &[177, 151, 189],
+        5 => &[112, 103, 145],
+        4 => &[78, 80, 122],
+        3 => &[54, 64, 102],
+        2 => &[29, 48, 87],
+        1 => &[16, 39, 79],
+        0 => &[3, 30, 71],
+        _ => unreachable!(),
+    }
+}
+
+fn save_heightmap_to_disk(map: &HeightMap, output: &Path) {
+    let mut buffer = ImageBuffer::new(map.width as u32, map.height as u32);
+
+    for (x, y, pixel) in buffer.enumerate_pixels_mut() {
+        let pos = map.xy_to_pos(x as usize, y as usize);
+        let state = map.data[pos];
+        *pixel = image::Rgb(*state_to_color(state));
+    }
+
+    buffer.save(output).expect("Could not generate heightmap.");
+}
+
 impl HeightMap {
     pub fn find_low_points(&self) -> Vec<u32> {
+        self.find_low_point_positions()
+            .iter()
+            .map(|&x| self.data[x])
+            .collect()
+    }
+
+    pub fn find_low_point_positions(&self) -> Vec<usize> {
         self.data
             .iter()
             .enumerate()
-            .filter_map(|(idx, &x)| {
+            .filter_map(|(idx, _x)| {
                 if self.value_lower_than_grid_neighbors(idx) {
-                    Some(x)
+                    Some(idx)
                 } else {
                     None
                 }
             })
             .collect()
+    }
+
+    pub fn find_basin_positions(&self) -> Vec<Vec<usize>> {
+        let positions = self.find_low_point_positions();
+        positions
+            .iter()
+            .map(|&x| self.discover_basin_at_position(x))
+            .collect()
+    }
+
+    pub fn largest_basin_mul(&self, basins: Vec<Vec<usize>>) -> usize {
+        let sorted: Vec<_> = basins.iter().map(|x| x.len()).sorted().rev().collect();
+        sorted.iter().take(3).product()
+    }
+
+    fn discover_basin_at_position(&self, position: usize) -> Vec<usize> {
+        let mut set = HashSet::new();
+        set.insert(position);
+        self.discover_basin_at_position_rec(position, &mut set);
+        set.into_iter().sorted().collect()
+    }
+
+    fn discover_basin_at_position_rec(&self, position: usize, found: &mut HashSet<usize>) {
+        // let this_value = self.data[position];
+        for npos in self.grid_neighbors_position(position) {
+            if found.contains(&npos) {
+                continue;
+            }
+
+            let other_value = self.data[npos];
+            if other_value < 9 {
+                found.insert(npos);
+                self.discover_basin_at_position_rec(npos, found);
+            }
+        }
     }
 
     pub fn compute_risk_level_sum(&self, points: &[u32]) -> u32 {
@@ -180,22 +261,29 @@ impl Challenge for Day09 {
 
     fn run_ex1(&mut self) -> String {
         let hm = HeightMap::from(parse_input_raw!());
+        let path = PathBuf::new().join("debug").join("day09.png");
+        save_heightmap_to_disk(&hm, &path);
+
         let points = hm.find_low_points();
         hm.compute_risk_level_sum(&points).to_string()
     }
 
     fn run_ex2(&mut self) -> String {
-        "?".to_string()
+        let hm = HeightMap::from(parse_input_raw!());
+        let basins = hm.find_basin_positions();
+        hm.largest_basin_mul(basins).to_string()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use crate::create_day_tests;
 
-    use super::HeightMap;
+    use super::{save_heightmap_to_disk, HeightMap};
 
-    create_day_tests!("09", "631", "?");
+    create_day_tests!("09", "631", "821560");
 
     const SAMPLE_DATA: &str = indoc::indoc! {"
         2199943210
@@ -208,6 +296,8 @@ mod tests {
     #[test]
     fn test_sample() {
         let hm = HeightMap::from(SAMPLE_DATA);
+        let path = PathBuf::new().join("debug").join("day09-sample.png");
+        save_heightmap_to_disk(&hm, &path);
         assert_eq!(hm.find_low_points(), &[1, 0, 5, 5]);
     }
 
@@ -215,5 +305,22 @@ mod tests {
     fn test_sample_sum() {
         let hm = HeightMap::from(SAMPLE_DATA);
         assert_eq!(hm.compute_risk_level_sum(&[1, 0, 5, 5]), 15);
+    }
+
+    #[test]
+    fn test_discover_basin() {
+        let hm = HeightMap::from(SAMPLE_DATA);
+        assert_eq!(hm.discover_basin_at_position(0), &[0, 1, 10]);
+        assert_eq!(
+            hm.discover_basin_at_position(9),
+            &[5, 6, 7, 8, 9, 16, 18, 19, 29]
+        );
+    }
+
+    #[test]
+    fn test_sample_basin() {
+        let hm = HeightMap::from(SAMPLE_DATA);
+        let basins = hm.find_basin_positions();
+        assert_eq!(hm.largest_basin_mul(basins), 1134);
     }
 }
